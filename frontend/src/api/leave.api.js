9f5dae562069c3e437e5fc931fd4028d9b60bee2
s777ia <23046128@myrp.edu.sg>
2026-01-01 03:02:18 +0800

    Added more api calls

--- frontend/src/api/leave.api.js
+++ frontend/src/api/leave.api.js
@@ -1,5 +1,6 @@
 import { authFetch } from "../utils/authFetch";
 
+// USER & ADMIN
 export async function fetchHistory() {
     try {
         const res = await authFetch('http://localhost:5000/api/leaves');
@@ -17,6 +18,7 @@ export async function fetchHistory() {
     }
 };
 
+// USER
 export async function addLeaveRequest(newRequest) {
     try {
         const res = await authFetch(`http://localhost:5000/api/leaves`, {
@@ -65,9 +67,51 @@ export async function fetchLeaveTypeName() {
         }
 
         return data;
-        
-      } catch (error) {
+
+    } catch (error) {
         console.error('Error fetching leave type name:', error);
         throw error;
-      }
+    }
 }
+
+// ADMIN
+export async function fetchRequestedHistory(leaveReq) {
+    const { id } = leaveReq;
+    try {
+        const res = await authFetch(`http://localhost:5000/api/leaves/${id}`);
+        const data = await res.json();
+
+        if (!res.ok) {
+            throw new Error(data.msg || 'Failed to fetch leave request history');
+        }
+
+        return data;
+
+    } catch (error) {
+        console.error('Error fetching leave request history:', error);
+    }
+}
+
+export async function updateLeaveRequest(request) {
+    const { id } = request;
+    try {
+        const res = await authFetch(`http://localhost:5000/api/leaves/${id}`, {
+            method: 'PATCH',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(request)
+        });
+
+        const data = await res.json();
+
+        if (!res.ok) {
+            throw new Error(data.msg || 'Failed to approve leave request');
+        }
+
+        return data;
+
+    } catch (error) {
+        throw error;
+    }
+} 
