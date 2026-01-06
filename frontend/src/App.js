7c28fb7ccbe7d7ec2176d0de1900df015c1bcf91
nicosimm <23009210@myrp.edu.sg>
2026-01-06 11:43:39 +0800

    Modify App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,24 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import NavBar from './components/NavBar';
-import TeamList from './pages/manager/TeamList';
-import './App.css';
 
-function App() {
+import EmployeeLeaveRequest from "./pages/employee/LeaveRequest"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import ManagerLeaveRequest from "./pages/manager/LeaveRequest"
+import ShiftRequestPage from "./pages/employee/ShiftRequestPage";
+import Home from "./pages/employee/Home";
+import Login from "./pages/auth/Login";
+import ProtectedRoute from "./components/ProtectedRoute";
+
+
+export default function App() {
   return (
     <Router>
-      <div className="app-layout">
-        <NavBar />
-        <div className="main-content">
-          <Routes>
-            <Route path="/manager/team-list" element={<TeamList />} />
-          </Routes>
-        </div>
-      </div>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/employee/home" element={<Home />} />
+        <Route path="/employee/requests/shift" element={<ProtectedRoute><ShiftRequestPage /></ProtectedRoute>} />
+        <Route path="/employee/requests/leave" element={<ProtectedRoute><EmployeeLeaveRequest /></ProtectedRoute>} />
+        <Route path="/manager/requests/leave" element={<ProtectedRoute><ManagerLeaveRequest /></ProtectedRoute>} />
+        {/* <EmployeeLeaveRequest /> */}
+      </Routes>
     </Router>
-  );
+  )
 }
-
-export default App;
